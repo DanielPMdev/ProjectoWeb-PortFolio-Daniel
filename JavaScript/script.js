fed8f9c19e1b5b84b1df6636337e5dd1a7fca6db
DanielPM <danielpm.dev@gmail.com>
2024-03-03 12:21:48 +0100

    Boton de Copiar Realizado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,21 +1,19 @@
-// Primero, selecciona el botón y el correo electrónico
 var boton = document.querySelector('.Email button');
 var correo = document.querySelector('.Email a');
+var notificacion = document.querySelector('#copyNotification');
 
-// Luego, añade un evento de click al botón
 boton.addEventListener('click', function() {
-    // Crea un elemento textarea temporal
     var tempElemento = document.createElement('textarea');
-    // Establece su valor al correo electrónico
     tempElemento.value = correo.textContent;
-    // Añade el textarea al cuerpo del documento
     document.body.appendChild(tempElemento);
-    // Selecciona el texto
     tempElemento.select();
-    // Copia el texto al portapapeles
     document.execCommand('copy');
-    // Elimina el textarea temporal
     document.body.removeChild(tempElemento);
-    // Notifica al usuario que el correo electrónico ha sido copiado
-    alert('Correo electrónico copiado al portapapeles: ' + correo.textContent);
+
+    // Muestra la notificación
+    notificacion.style.display = 'inline';
+    setTimeout(function() {
+        notificacion.style.display = 'none';
+    }, 2000);
 });
+
