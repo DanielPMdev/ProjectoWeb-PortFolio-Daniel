fee6c894b685def528dd620c6286ad19f3f6c057
DanielPM <danielpm.dev@gmail.com>
2024-03-03 14:34:38 +0100

    Menu creado y implementado

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,3 +17,41 @@ boton.addEventListener('click', function() {
     }, 2000);
 });
 
+
+// Menu Desplegable
+// Primero, selecciona el icono del menú y el menú móvil
+var menuIcono = document.querySelector('.menu');
+var menuMovil = document.querySelector('.movil--menu');
+var enlacesMenu = document.querySelectorAll('.movilmenu--links a');
+
+// Luego, agrega un evento de clic al icono del menú
+menuIcono.addEventListener('click', function() {
+    // Comprueba si el menú móvil ya se está mostrando
+    if (menuMovil.style.display === 'block') {
+        // Si es así, oculta el menú móvil y permite el desplazamiento del cuerpo
+        menuMovil.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    } else {
+        // Si no, muestra el menú móvil y oculta el desplazamiento del cuerpo
+        menuMovil.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    }
+});
+
+// También puedes agregar un evento de clic al botón de cierre del menú móvil
+var botonCerrar = document.querySelector('.movilmenu--botoncerrar');
+botonCerrar.addEventListener('click', function() {
+    // Cuando se hace clic en el botón de cierre, cambia la propiedad display del menú móvil a none
+    menuMovil.style.display = 'none';
+    document.body.style.overflow = 'auto'
+});
+
+// Finalmente, agrega un evento de clic a cada enlace en el menú móvil
+for (var i = 0; i < enlacesMenu.length; i++) {
+    enlacesMenu[i].addEventListener('click', function() {
+        // Cuando se hace clic en un enlace, oculta el menú móvil y permite el desplazamiento del cuerpo
+        menuMovil.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+}
+
